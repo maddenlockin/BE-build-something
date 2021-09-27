c7452651475abe555c78c3cc4fdf2707bde2d187
maddenlockin <maddenlockin@gmail.com>
2021-09-27 08:26:40 -0700

    attempting debug- not passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,30 +3,31 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+describe('routes', () => {
+    beforeEach(() => {
+        return setup(pool);
+    });
+  
+    it('POSTS a new material', async () => {
+        const currentDate = new Date().toISOString().slice(0, 10);
+        const material = {
+            date: currentDate, 
+            material: 'linen',
+            piece: 'pants',
+            color: 'cream',
+            have: true
+        };
 
-  it('POSTS a new material', aysnc () => {
-    const material = {
-      material: 'linen',
-      piece: 'pants',
-      color: 'cream',
-      have: true,
-    };
-    const res = await (request(app).post('api/v1/clothing-inventory')).send(material);
-
-    expect(res.body).toEqual({
-      id: '1',
-      date: '04-01-2021',
-      ...material,
-    })
-  });
-
-  afterAll(() => {
-    pool.end();
-  });
-
+        const res = await request(app)
+            .post('api/v1/clothing-inventory')
+            .send(material);
+        console.log(res);
+        expect(res.body).toEqual({
+            id: '1',
+            date: '09-27-2021',
+            ...material,
+        });
+    });
 });
+
 
